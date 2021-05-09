6e09e8588ab2579d47f5c46e53550f5604917ddb
saadabrar12 <saad6522@gmail.com>
2021-05-09 19:50:18 -0400

    Updated infer_compress_ai.py

--- models/infer_compress_ai.py
+++ models/infer_compress_ai.py
@@ -2,6 +2,7 @@ import math
 import io
 import torch
 from torchvision import transforms
+import torch.nn.functional as F
 import numpy as np
 
 from PIL import Image
@@ -15,13 +16,76 @@ from ipywidgets import interact, widgets
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
-net = bmshj2018_hyperprior(quality=2, pretrained=True).eval().to(device)
-print(f'Parameters: {sum(p.numel() for p in net.parameters())}')
-
-img = Image.open('./rome.png').convert('RGB')
-generate_reconstructed_image(img)
-
 #%matplotlib inline
+
+def pad(x, p=2 ** 6):
+    h, w = x.size(2), x.size(3)
+    H = (h + p - 1) // p * p
+    W = (w + p - 1) // p * p
+    padding_left = (W - w) // 2
+    padding_right = W - w - padding_left
+    padding_top = (H - h) // 2
+    padding_bottom = H - h - padding_top
+    return F.pad(
+        x,
+        (padding_left, padding_right, padding_top, padding_bottom),
+        mode="constant",
+        value=0,
+    )
+
+def crop(x, h,w):
+    H, W = x.size(2), x.size(3)
+    #h, w = size
+    padding_left = (W - w) // 2
+    padding_right = W - w - padding_left
+    padding_top = (H - h) // 2
+    padding_bottom = H - h - padding_top
+    return F.pad(
+        x,
+        (-padding_left, -padding_right, -padding_top, -padding_bottom),
+        mode="constant",
+        value=0,
+    )
+
+def torch2img(x: torch.Tensor) -> Image.Image:
+    return transforms.ToPILImage()(x.clamp_(0, 1).squeeze())
+
+
+def _encode(img, net):
+    x = transforms.ToTensor()(img).unsqueeze(0).to(device)
+    
+    h, w = x.size(2), x.size(3)
+    p = 64  # maximum 6 strides of 2
+    x = pad(x, p)
+    
+    with torch.no_grad():
+        out = net.compress(x)
+    
+    #keys are strings and shape
+
+    return out
+
+def _decode(out_tensor, original_img, net):
+    o_img = transforms.ToTensor()(original_img).unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        out = net.decompress(out_tensor["strings"], out_tensor["shape"])
+    
+    h, w = o_img.size(2), o_img.size(3)
+    x_hat = crop(out["x_hat"], h,w)
+    img = torch2img(x_hat)
+    
+    img.save("Decoded_image.png")  
+    return x_hat 
+
+def forward(img_path):
+    net = bmshj2018_hyperprior(quality=2, pretrained=True).eval().to(device)
+    print(f'Parameters: {sum(p.numel() for p in net.parameters())}')
+
+    img = Image.open(img_path).convert('RGB')
+
+    encoded = _encode(img, net) # return torch tensor
+    decoded = _decode(encoded, img, net) # return torch tensor
 
 
 def generate_reconstructed_image(img):
@@ -59,3 +123,5 @@ def generate_reconstructed_image(img):
     plt.show()
     
     return rec_net
+
+forward("./rome.png")
